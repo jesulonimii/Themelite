d5b83a3f196a17be2a8f440b835b74242b28e8d7
Your NameerliteHq <erlite.team@gmail.com>
2021-01-21 15:40:01 +0100

    Activate Library

---
+++ themelite/src/main/java/com/erlite/themelite/ThemeLite.java
@@ -0,0 +1,35 @@
+package com.erlite.themelite;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class ThemeLite {
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    int setTheme;
+
+    public ThemeLite(Context context){
+
+        sharedPreferences = context.getSharedPreferences("darkLite", Context.MODE_PRIVATE);
+
+        //fetch theme for shared prefs
+        setTheme = sharedPreferences.getInt("ThemeValue", 1);
+
+    }
+
+    //save the theme state
+    public void saveTheme(Context context){
+
+        setTheme = AppCompatDelegate.getDefaultNightMode();
+
+        sharedPreferences = context.getSharedPreferences("darkLite", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+        editor.putInt("ThemeValue", setTheme);
+        editor.apply();
+
+    }
+}
